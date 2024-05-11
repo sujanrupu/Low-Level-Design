d5d7e405c13a011dd25650013f0d707a2bb19d03
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2024-05-11 15:49:56 +0530

    Create Swift.java

---
+++ Car_purchase_system/Swift.java
@@ -0,0 +1,20 @@
+public class Swift{
+    public void start_prod() {
+        System.out.println("Starting production of Swift");
+    }
+    public void init() {
+        System.out.println("Assembling configuration for swift");
+    }
+    public void buildEngine() {
+        System.out.println("Building swift engine");
+    }
+    public void buildBody() {
+        System.out.println("Building swift body");
+    }
+    public void buildChassis() {
+        System.out.println("Building swift chassis");
+    }
+    public void buildTyre() {
+        System.out.println("Building swift tyre");
+    }
+}
