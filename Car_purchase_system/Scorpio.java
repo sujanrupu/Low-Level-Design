2fc9cd366e136f7a85f6a9f9ca9ca3e98e81db65
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2024-05-11 15:49:42 +0530

    Create Scorpio.java

---
+++ Car_purchase_system/Scorpio.java
@@ -0,0 +1,20 @@
+public class Scorpio{
+    public void start_prod() {
+        System.out.println("Starting production of Scorpio");
+    }
+    public void init() {
+        System.out.println("Assembling configuration for scorpio");
+    }
+    public void buildEngine() {
+        System.out.println("Building scorpio engine");
+    }
+    public void buildBody() {
+        System.out.println("Building scorpio body");
+    }
+    public void buildChassis() {
+        System.out.println("Building scorpio chassis");
+    }
+    public void buildTyre() {
+        System.out.println("Building scorpio tyre");
+    }
+}
