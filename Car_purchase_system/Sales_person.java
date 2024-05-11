f40174f04c791858607ca847acc2c3a3f4133d5a
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2024-05-11 15:50:11 +0530

    Create Sales_person.java

---
+++ Car_purchase_system/Sales_person.java
@@ -0,0 +1,13 @@
+public class Sales_person extends Car_building{
+    public void confirmation() {
+        System.out.println("Confirmed order from customer");
+    }
+    @Override
+    public void Scorpio_build(){
+        super.Scorpio_build();
+    }
+    @Override
+    public void Swift_build(){
+        super.Swift_build();
+    }
+}
