9ad9787b99144c085ffa3d185a127dfcfc54dad1
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2024-06-01 15:33:26 +0530

    Delete Builder Pattern directory

--- Builder Pattern/ICarBuilder.java
+++
@@ -1,9 +0,0 @@
-import Models.ICar;
-
-public interface ICarBuilder {
-    public void buildEngine();
-    public void buildBody();
-    public void buildChassis();
-    public void buildTyre();
-    public ICar build();
-}
